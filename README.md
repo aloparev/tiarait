1491d75a2f9f10f6cc7678d063714d06aa12ae74
LeoRoi <kuzne4eg@gmail.com>
2020-06-03 20:59:15 +0200

    add server lib dep

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # tiarait
+
+## run
+```
+Start des Servers: 
+java -Djava.library.path=repo/htw/ai/lenz/tiarait-server/1/lib/native/ -jar repo/htw/ai/lenz/tiarait-server/1/tiarait-server-1.jar
+```
--- src/main/java/TiaraitClient.java
+++ src/main/java/TiaraitClient.java
@@ -1,7 +1,35 @@
+import lenz.htw.tiarait.ColorChange;
+import lenz.htw.tiarait.net.NetworkClient;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class TiaraitClient
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Java:" + System.getProperty("java.version"));
+        NetworkClient networkClient = new NetworkClient(null, "abeta");
+
+        networkClient.getMyPlayerNumber(); // 0-3 (ACHTUNG! andere Nummerierung als beim ColorChange)
+
+        while (networkClient.isAlive()) {
+// steuerung
+//            float x = networkClient.getX(player, botNr);
+//            float y = networkClient.getY(player, botNr);
+
+            networkClient.isWall(7, 11); //true wenn bei Koordinate 7,11 ein Hindernis steht
+
+            networkClient.setMoveDirection(0, 0.1f, -0.8f);
+            networkClient.setMoveDirection(1, 0.1f, 1.8f);
+            networkClient.setMoveDirection(2, -5.1f, -0.8f);
+
+            ColorChange cc;
+            while ((cc = networkClient.getNextColorChange()) != null) {
+// cc in eigene Struktur einarbeiten
+//                z.B. brett[cc.x][cc.y] = cc.newColor;
+//                cc.newColor; //0 = leer, 1-4 = spieler
+                log.info("cc=" + cc);
+            }
+        }
     }
 }
